d7e32fb3fa031518b27d7440dae59cc9d69809dd
TANGKUO <616507752@qq.com>
2017-06-17 15:52:55 +0800

    add

--- src/test/java/com/tk/cn/io/TestBR.java
+++ src/test/java/com/tk/cn/io/TestBR.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-public class TestBR {
+public class TestBR { 
 	public static void main(String[]args)
 	throws Exception{
 		FileInputStream fis=new FileInputStream("/home/tk/a.txt");
